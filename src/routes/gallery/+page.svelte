2bbd1ed8b5ba592d807f1e00b768bb6586b54d6d
Chris Tennant <chris>
2023-01-29 16:18:21 +0000

    add images

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -3,8 +3,21 @@
 	<meta name="Childminder Skelton Gallery" content="Gallery and images" />
 </svelte:head>
 
-<div>
-	Hello!!
-</div>
+<section id="content">
+	<div id="image-section">
+		<img
+			src="/src/lib/images/gallery/header.jpg"
+			alt="children down at Skelton pond"
+			class="mx-auto pt-4"
+		/>
+	</div>
+
+	<div class="grid grid-cols-1 md:grid-cols-1 gap-1 pt-5 px-2 md:px-6">
+		<div class="text-center">
+			<h1>Gallery</h1>
+		</div>
+
+	</div>
+</section>
 
 
