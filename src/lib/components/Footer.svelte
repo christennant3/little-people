773af2662f675ff2560a39c79dc69b2f2266c2ac
Chris Tennant <chris>
2023-03-03 14:14:59 +0000

    add footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,15 +1,32 @@
 <script>
-
 </script>
 
 <footer>
-    <div class="footer-bg">
-    <p class="p-2">Footer</p>
+    <div class="footer-bg text-">
+        <div class="grid grid-cols-6 md:grid-cols-6">
+            <div class="col-span-3 flex items-center">
+                <div class="p-2">
+                    <p>&copy; Little Peoples Childminding</p>
+                </div>
+            </div>
+            <div class="col-span-3 flex justify-end items-center">
+                Please visit my Facebook page: 
+                <a
+                    href="https://www.facebook.com/people/Little-Peoples-Childminding/100063605529490/"
+                    ><img
+                        src="/src/lib/images/social/facebook-grey.png"
+                        alt="facebook logo" class="p-3"
+                        
+                    /></a
+                >
+            </div>
+        </div>
     </div>
 </footer>
 
 <style lang="scss" scoped>
     .footer-bg {
-       background-color: #336699;
+        background-color: #336699;
+        color: white;
     }
 </style>
