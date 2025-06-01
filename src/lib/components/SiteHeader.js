a4d302917c6ef22d93047872936137320c2e3512
Chris Tennant <chris>
2025-06-01 15:40:05 +0100

    fix: add missing "Contact" link in SiteHeader component

--- src/lib/components/SiteHeader.js
+++ src/lib/components/SiteHeader.js
@@ -5,6 +5,7 @@ class SiteHeader extends HTMLElement {
           <nav>
             <a href="/">Home</a>
             <a href="/about">About</a>
+            <a href="/contact">Contact</a>
           </nav>
         </header>
       `;
